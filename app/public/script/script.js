d7b2e55084918baa67698726642f255557198bcd
Ber-Max <max-_-b@hotmail.fr>
2019-07-06 20:00:10 +0200

    Amelioration Header

--- app/public/script/script.js
+++ app/public/script/script.js
@@ -1,15 +1,28 @@
 var profil = document.querySelector(".header_profil");
 var profilDropdown = document.querySelector(".profil_dropdown");
+var html = document.querySelector("html");
+var profilElement1 = document.getElementsByClassName("profil_element")[0];
+var profilElement2 = document.getElementsByClassName("profil_element")[1];
 
 profilDropdown.classList.add("nonedisplay");
 
-profil.addEventListener("click", function() {
+/* profil.addEventListener("click", function() {
   if (profilDropdown.classList.contains("nonedisplay")) {
     profilDropdown.classList.remove("nonedisplay");
     profilDropdown.classList.add("flexdisplay");
-  } else {
-    console.log("Plop");
+  }
+}); */
+
+html.addEventListener("click", function(event) {
+/*   if (profilDropdown.classList.contains("flexdisplay")) {
     profilDropdown.classList.remove("flexdisplay");
     profilDropdown.classList.add("nonedisplay");
+  } */
+  if (event.target === profil) {
+    profilDropdown.classList.toggle("nonedisplay");
+    profilDropdown.classList.toggle("flexdisplay");
+  } else if (event.target != profilDropdown && event.target != profilElement1 && event.target != profilElement2) {
+    profilDropdown.classList.add("nonedisplay");
+    profilDropdown.classList.remove("flexdisplay");
   }
 });
