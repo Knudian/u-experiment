d0358cc84629863f27e30dcdfa96a3b02517ccda
6mon <simon.genois@imie.fr>
2019-07-08 09:58:45 +0200

    récupération css

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -13,24 +13,61 @@ class MainController extends AbstractController
     {
         $page_title = 'La page';
         // liste des intitulés de domaines professionnels
-        $domains = [
+        $list_domain = [
           'domaine1',
           'domaine2',
           'Maxime appartient à Simon',
           'Simon appartient à Maxime',
           'Mais ils ne le savent pas ...'
         ];
-        $jobs = [
-          '1',
-          '2'
+        // liste des métiers
+        $list_job = [
+          'boulanger',
+          'ébéniste',
+          'je-m\'en-foutiste',
+          'marsouin'
+        ];
+        // liste des niveaux d'entrée
+        $list_lvlin = [
+          'CAP',
+          'BAC',
+          'BAC+52',
+          'Brevet des collèges'
+        ];
+        // liste des niveaux de sortie
+        $list_lvlout = [
+            'CAP',
+            'BAC',
+            'BAC+52',
+            'Brevet des collèges'
+        ];
+        // liste des dernières formations ajoutées
+        $lastAdded = [
+            [
+                'date' => '01/01/1991',
+                'title' => 'plombiste spécialisationné en plomberies',
+                'domain' => 'btp'
+            ]
+        ];
+        // liste des dernières formations ajoutées
+        $lastUpdated = [
+            [
+                'date' => '01/01/1991',
+                'title' => 'plombiste spécialisationné en plomberies',
+                'domain' => 'btp'
+            ]
         ];
 
         return $this->render(
           'index.html.twig',
           [
             'page_title' => $page_title,
-            'domains' => $domains,
-            'jobs' => $jobs
+            'list_domain' => $list_domain,
+            'list_job' => $list_job,
+            'lastAdded' => $lastAdded,
+            'lastUpdated' => $lastUpdated,
+            'list_lvlin' => $list_lvlin,
+            'list_lvlout' => $list_lvlout
           ]
         );
     }
