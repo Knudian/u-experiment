ec59c50bc777d8b6f01e2734ec26c690abe55d8d
Knud <kevin.meche.pro@gmail.com>
2019-07-09 07:23:44 +0200

    Adding RecruitmentStatus

---
+++ app/src/Entity/RecruitmentStatus.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\RecruitmentStatusRepository")
+ */
+class RecruitmentStatus
+{
+    /**
+     * @var id
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=20)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
